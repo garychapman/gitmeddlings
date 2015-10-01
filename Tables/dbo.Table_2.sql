45e02818d597dfef70932b5b19f6dac3cba43827
garychapman@iprimus.com.au <>
2015-10-01 11:20:43 +1000

    Changed column name

--- Tables/dbo.Table_2.sql
+++ Tables/dbo.Table_2.sql
@@ -3,7 +3,7 @@ SET QUOTED_IDENTIFIER ON
 SET ANSI_PADDING ON
 GO
 CREATE TABLE [dbo].[Table_2] (
-		[Col]     [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+		[Column]     [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Table_2] SET (LOCK_ESCALATION = TABLE)
